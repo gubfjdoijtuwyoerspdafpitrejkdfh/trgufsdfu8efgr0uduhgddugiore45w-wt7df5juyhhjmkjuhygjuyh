03db568ae25a06bc3ab90259cc824f00b031d4f4
Mohamed_Ashraf <46439491+gubfjdoijtuwyoerspdafpitrejkdfh@users.noreply.github.com>
2019-01-13 15:24:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -692,10 +692,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('&send')){
- if (message.author.id !== '427919028934279179
-') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '427919028934279179
-') return;
+ if (message.author.id !== '427919028934279179') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '427919028934279179') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
