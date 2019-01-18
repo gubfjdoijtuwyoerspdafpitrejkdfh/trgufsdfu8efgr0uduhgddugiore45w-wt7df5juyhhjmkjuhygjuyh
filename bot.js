073574165ca2f36b9db39ce0dc30d240f405251f
Mohamed_Ashraf <46439491+gubfjdoijtuwyoerspdafpitrejkdfh@users.noreply.github.com>
2019-01-18 11:26:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -774,22 +774,6 @@ message.author.sendEmbed(embed)
 }
 }); 
 
-
-
-
-
-lient.on('message', message => {//new msg event
-if(!message.channel.guild) return;
-  if(message.content.startsWith(prefix + 'Rainbow')) {//to create the rainbow role
-      let role = message.guild.roles.find('name', 'Rainbow')
-    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
-  //start of create role 
-  if(!role){
-    rainbow =  message.guild.createRole({
-   name: "Rainbow",//the role will create name
-   color: "#000000",//the default color
-   permissions:[]//the permissions
- //end of create role
 })
 
 }
